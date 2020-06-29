303b17e87d669a6bfe565196bf9547693f7fe953
sich97 <sich97@gmail.com>
2020-06-29 08:59:19 +0200

    Initial setup

---
+++ server.py
@@ -0,0 +1,14 @@
+"""
+File: server.py
+
+This program receives settings from the client (including wakeup time).
+It also starts the alarm on the given wakeup time, and waits for an awoke signal from the client before stopping it.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
